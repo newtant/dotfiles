708b050590e1d90d548cf971b60f5f1a3a5ebdf9
newtant <the.newtant@gmail.com>
2018-04-22 15:40:10 +0200

    changes

--- scripts/trackpoint.sh
+++ scripts/trackpoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -n 150 > /sys/devices/platform/i8042/serio1/serio2/sensitivity
-echo -n 75 > /sys/devices/platform/i8042/serio1/serio2/speed
+echo -n 150 > /sys/devices/platform/i8042/serio1/sensitivity
+echo -n 100 > /sys/devices/platform/i8042/serio1/speed
