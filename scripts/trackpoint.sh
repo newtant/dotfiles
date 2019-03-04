f9d7afc856c6a29ec697fe703b9d1927d3eec022
newtant <the.newtant@gmail.com>
2019-03-04 22:02:39 +0100

    fiddled around

--- scripts/trackpoint.sh
+++ scripts/trackpoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -n 150 > /sys/devices/platform/i8042/serio1/sensitivity
+echo -n 160 > /sys/devices/platform/i8042/serio1/sensitivity
 echo -n 100 > /sys/devices/platform/i8042/serio1/speed
---
+++ scripts/view.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xfd -fn '-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1'
