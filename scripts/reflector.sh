4ca22fb22bc86fb1a4b0490629ffd36b59b40638
newtant <the.newtant@gmail.com>
2019-04-28 14:28:43 +0200

    message

--- scripts/reflector.sh
+++ scripts/reflector.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo reflector --country "Germany" --verbose --latest 40 --number 10 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
+sudo reflector --country "Germany" --verbose --latest 10 --number 10 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
--- scripts/trackpoint.sh
+++ scripts/trackpoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo -n 160 > /sys/devices/platform/i8042/serio1/sensitivity
-echo -n 100 > /sys/devices/platform/i8042/serio1/speed
+echo -n 200 > /sys/devices/platform/i8042/serio1/sensitivity
+echo -n 65 > /sys/devices/platform/i8042/serio1/speed
+echo -n 1 > /sys/devices/platform/i8042/serio1/press_to_select
+echo "Loaded trackpoint settings successfully."
