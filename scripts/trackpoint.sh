05494dc46ee14ed64d4487ac570867f0c228df6f
newtant <the.newtant@gmail.com>
2020-02-01 20:26:48 +0100

    been a while, updated

--- scripts/trackpoint.sh
+++ scripts/trackpoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -n 200 > /sys/devices/platform/i8042/serio1/sensitivity
+echo -n 150 > /sys/devices/platform/i8042/serio1/sensitivity
 echo -n 65 > /sys/devices/platform/i8042/serio1/speed
 echo -n 1 > /sys/devices/platform/i8042/serio1/press_to_select
 echo "Loaded trackpoint settings successfully."
